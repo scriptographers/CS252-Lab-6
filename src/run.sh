bd34a1fb18d093512a38c83bb3b3aa1da7e83884
devansh-dvj <devansh.dvj@gmail.com>
2021-05-07 21:58:29 +0530

    Updated run.sh - Added wait and file randomization and file comparison

--- src/run.sh
+++ src/run.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
+dd if=/dev/urandom bs=102400 count=1 status=none | base64 >send.txt
+
 # Compile:
 
-gcc -Wall -Wextra -o c.out client.c 
+gcc -Wall -Wextra -o c.out client.c
 
 if [ $? -eq 0 ]; then
     echo "Client compilation successful"
@@ -25,6 +27,21 @@ echo ""
 # Run
 ./s.out &
 ./c.out &
+
+wait
+
+echo ""
+
+STATUS="$(
+    cmp --silent send.txt recv.txt
+    echo $?
+)" # "$?" gives exit status for each comparison
+
+if [[ $STATUS -ne 0 ]]; then # if status isn't equal to 0, then execute code
+    echo "Different files"
+else
+    echo "Same files"
+fi
 
 # Clean up
 rm s.out
