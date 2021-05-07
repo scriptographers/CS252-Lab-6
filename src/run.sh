e5702880b90462709c73c9eb83884b2c5ceb8efa
Harshit Varma <varmaharshit2@gmail.com>
2021-05-07 20:02:50 +0530

    Renamed bash script

--- src/run.sh
+++ src/run.sh
@@ -1,25 +1,29 @@
 #!/bin/bash
 
-# Usage: bash run.sh <Senders Port> <Receivers Port> <Number of packets> <Timeout> <Drop probability>
-# Example: "bash run.sh 8080 8000 3 1 0.2"
-
-N_ARGS=$#
-if [ $N_ARGS -ne 5 ] 
-then
-    echo "Usage: bash $0 <Senders Port> <Receivers Port> <Number of packets> <Timeout> <Drop probability>"
-    exit 1
-fi 
-
-SPORT=$1
-RPORT=$2
-N_PKTS=$3
-TIMEOUT=$4
-DROP_PROB=$5
-
 # Compile:
-gcc -o s.out sender.c
-gcc -o r.out receiver.c
 
-# Run:
-./r.out $RPORT $SPORT $DROP_PROB & # Start the receiver
-./s.out $SPORT $RPORT $TIMEOUT $N_PKTS & # Start the sender in parallel
+gcc -Wall -Wextra -o c.out client.c 
+
+if [ $? -eq 0 ]; then
+    echo "Client compilation successful"
+else
+    echo "Client compilation error"
+    exit $?
+fi
+
+gcc -Wall -Wextra -o s.out server.c
+
+if [ $? -eq 0 ]; then
+    echo "Server compilation successful"
+else
+    echo "Server compilation error"
+    exit $?
+fi
+
+# Run
+./s.out &
+./c.out &
+
+# Clean up
+rm s.out
+rm c.out
