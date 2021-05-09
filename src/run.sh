13487d688fd37738b8fc23a8f67a54dc25ea2444
devansh-dvj <devansh.dvj@gmail.com>
2021-05-09 14:51:22 +0530

    Added compilation flag to run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,19 +1,20 @@
 #!/bin/bash
 
-if [ $# -ne 3 ]; then
-    echo "Usage: bash run.sh <congestion_protocol> <flag_gen_file> <file_size>"
+if [ $# -ne 4 ]; then
+    echo "Usage: bash run.sh <congestion_protocol> <flag_gen_file> <file_size> <flag_compile>"
     exit 1
 fi
 
 TCP_CONG_PROT=$1 # "reno" or "cubic"
-FLAG_GEN=$2 # 0 or 1
-FILE_SIZE=$3 # 100KB, 5MB etc
+FLAG_GEN=$2      # 0 or 1
+FILE_SIZE=$3     # 100KB, 5MB etc
+FLAG_COM=$4      # 0 or 1
 
 if [ $FLAG_GEN -eq 1 ]; then
 
-    # Generate the send file
-    base64 /dev/urandom | head -c $FILE_SIZE > send.txt
-    
+    # Generate send.txt
+    base64 /dev/urandom | head -c $FILE_SIZE >send.txt
+
     if [ $? -eq 0 ]; then
         echo "File generation successful"
     else
@@ -22,28 +23,32 @@ if [ $FLAG_GEN -eq 1 ]; then
     fi
 
 fi
-    
-# Compile:
 
-gcc -Wall -Wextra -o c.out client.c
+if [ $FLAG_COM -eq 1 ]; then
 
-if [ $? -eq 0 ]; then
-    echo "Client compilation successful"
-else
-    echo "Client compilation error"
-    exit $?
+    # Compile client:
+    gcc -Wall -Wextra -o c.out client.c
+
+    if [ $? -eq 0 ]; then
+        echo "Client compilation successful"
+    else
+        echo "Client compilation error"
+        exit $?
+    fi
+
+    # Compile server:
+    gcc -Wall -Wextra -o s.out server.c
+
+    if [ $? -eq 0 ]; then
+        echo "Server compilation successful"
+    else
+        echo "Server compilation error"
+        exit $?
+    fi
+
+    echo ""
+
 fi
-
-gcc -Wall -Wextra -o s.out server.c
-
-if [ $? -eq 0 ]; then
-    echo "Server compilation successful"
-else
-    echo "Server compilation error"
-    exit $?
-fi
-
-echo ""
 
 # Run
 ./s.out &
@@ -64,8 +69,7 @@ wait $P2 || let "FAIL2=1"
 
 echo ""
 
-if [ "$FAIL1" == "0" ] && [ "$FAIL2" == "0" ];
-then
+if [ "$FAIL1" == "0" ] && [ "$FAIL2" == "0" ]; then
     echo "Processes finished successfully"
 else
     echo "Process(es) failed: $FAIL1 $FAIL2"
@@ -78,12 +82,17 @@ STATUS="$(
     echo $?
 )" # "$?" gives exit status for each comparison
 
-if [[ $STATUS -ne 0 ]]; then # if status isn't equal to 0, then execute code
+# if status isn't equal to 0, then different files
+if [[ $STATUS -ne 0 ]]; then
     echo "Different files"
 else
     echo "Same files"
 fi
 
-# Clean up
-rm s.out
-rm c.out
+if [ $FLAG_COM -eq 1 ]; then
+
+    # Clean up
+    rm s.out c.out
+    echo "Clean up complete"
+
+fi
