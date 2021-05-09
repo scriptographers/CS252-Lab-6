40cfd5bac8c462bbc7e9a2969c34aa24f7d5b83b
devansh-dvj <devansh.dvj@gmail.com>
2021-05-09 18:41:57 +0530

    Added plots for experiment conducted

--- src/confidence_intervals.py
+++ src/confidence_intervals.py
@@ -5,12 +5,11 @@ import pandas as pd
 import seaborn as sns
 
 sns.set_theme(style="darkgrid")
-sns.set(rc={'figure.figsize': (16, 12)})
+sns.set(rc={'figure.figsize': (12, 10)})
 
 data = {'Congestion Protocol': [], 'Delay': [], 'Loss': [], 'Throughput (in kbps)': []}
 
 for tcp in ['TCP-Reno', 'TCP-Cubic']:
-
     t = open('{}.txt'.format(tcp[4:])).readlines()
 
     for i in range(9):
@@ -18,6 +17,7 @@ for tcp in ['TCP-Reno', 'TCP-Cubic']:
             data['Congestion Protocol'].append(tcp)
             data['Delay'].append(t[21 * i].split()[0])
             data['Loss'].append(t[21 * i].split()[1])
+            # Regex to get time taken
             time = float((re.findall(r'\d+\.\d+', t[21 * i + j + 1]))[0])
             data['Throughput (in kbps)'].append(5 * 1024 * 8 / time)
 
