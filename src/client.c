f020180007b8a45da30d2e489f1fd877d89ad9b3
devansh-dvj <devansh.dvj@gmail.com>
2021-05-09 15:55:05 +0530

    Reordered headers and formatted

--- src/client.c
+++ src/client.c
@@ -1,175 +1,159 @@
-#include <stdio.h> // For IO operations
+#include <stdio.h>  // For IO operations
 #include <stdlib.h> // Standard Library
-#include <unistd.h> // Standard POSIX functions, like "close"
 #include <string.h> // String manipulation for string packets etc
-#include <sys/time.h> // For timeval struct
+#include <unistd.h> // Standard POSIX functions, like "close"
+
+#include <sys/sendfile.h>
 #include <sys/socket.h> // For socket programming
-#include <arpa/inet.h> // Provides definitions for internet operations
+#include <sys/stat.h>
+#include <sys/time.h> // For timeval struct
+#include <sys/types.h>
+
+#include <arpa/inet.h>   // Provides definitions for internet operations
 #include <netinet/tcp.h> // Includes definitions for TCP
 
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <sys/sendfile.h>
-
 /* Constants */
-int status; // Used for error handling
+int status;            // Used for error handling
 const int SIZE = 1024; // in bytes
-const char* LOCAL_HOST = "127.0.0.1"; // Standard address for IPv4 loopback traffic
+const char *LOCAL_HOST =
+    "127.0.0.1"; // Standard address for IPv4 loopback traffic
 // int const SIZE = 1024; // gives a weird error for some reason
 int RECV_PORT = 5432;
 char *PATH_SEND = "send.txt";
 
-int main(int argc, char *argv[]){
+int main(int argc, char *argv[]) {
 
-    /* Get args */
-    // Check no. of args specified:
-    if (argc != 2){ // 1+4 args are needed
-        printf("Usage: %s <congestion_protocol>\n", argv[0]);
-        exit(EXIT_FAILURE);
-    }
-    const char *TCP_CONG_TYPE = argv[1];
+  /* Get args */
+  // Check no. of args specified:
+  if (argc != 2) { // 1+4 args are needed
+    printf("Usage: %s <congestion_protocol>\n", argv[0]);
+    exit(EXIT_FAILURE);
+  }
+  const char *TCP_CONG_TYPE = argv[1];
 
-    /* Create a TCP socket */
+  /* Create a TCP socket */
 
-    // Active open
-    int sockfd = socket(
-        AF_INET, // IPv4
-        SOCK_STREAM, // Specifies TCP socket
-        IPPROTO_TCP // TCP protocol, setting this to 0 also works
-    );
-    if (sockfd < 0){
-        perror("(Client) Error while creating the socket");
-        exit(EXIT_FAILURE);
-    }
-    printf("(Client) Socket created successfully\n");
+  // Active open
+  int sockfd = socket(AF_INET,     // IPv4
+                      SOCK_STREAM, // Specifies TCP socket
+                      IPPROTO_TCP  // TCP protocol, setting this to 0 also works
+  );
+  if (sockfd < 0) {
+    perror("(Client) Error while creating the socket");
+    exit(EXIT_FAILURE);
+  }
+  printf("(Client) Socket created successfully\n");
 
-    // Set sock options to completely close the socket (addr + port), for immediate reuse:
-    int reuse_flag = 1;
+  // Set sock options to completely close the socket (addr + port), for
+  // immediate reuse:
+  int reuse_flag = 1;
 
-    status = setsockopt(
-        // The socket:
-        sockfd,
-        // The socket layer, more info: https://stackoverflow.com/questions/21515946/what-is-sol-socket-used-for
-        SOL_SOCKET,
-        // This option allows your server to bind to an address which is in a TIME_WAIT state 
-        SO_REUSEADDR,
-        // Setting to true
-        &reuse_flag,
-        sizeof(int)
-    );
-    if (status != 0){
-        perror("(Client) An error occured while setting the socket options");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
+  status = setsockopt(
+      // The socket:
+      sockfd,
+      // The socket layer, more info:
+      // https://stackoverflow.com/questions/21515946/what-is-sol-socket-used-for
+      SOL_SOCKET,
+      // This option allows your server to bind to an address which is in a
+      // TIME_WAIT state
+      SO_REUSEADDR,
+      // Setting to true
+      &reuse_flag, sizeof(int));
+  if (status != 0) {
+    perror("(Client) An error occured while setting the socket options");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
 
-    status = setsockopt(
-        sockfd,
-        SOL_SOCKET,
-        SO_REUSEPORT,
-        &reuse_flag,
-        sizeof(int)
-    );
-    if (status != 0){
-        perror("(Client) An error occured while setting the socket options");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
+  status =
+      setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse_flag, sizeof(int));
+  if (status != 0) {
+    perror("(Client) An error occured while setting the socket options");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
 
-    // Set sock options to specify Reno or Cubic
-    status = setsockopt(
-        sockfd,
-        IPPROTO_TCP,
-        TCP_CONGESTION,
-        TCP_CONG_TYPE,
-        strlen(TCP_CONG_TYPE)
-    );
-    if (status != 0){
-        perror("(Client) An error occured while setting the socket options");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-    
-    // Set the SO_LINGER sock option
-    struct linger so_linger;
-    so_linger.l_onoff = 1;
-    so_linger.l_linger = 10;
-    status = setsockopt(
-        sockfd,
-        SOL_SOCKET,
-        SO_LINGER,
-        &so_linger,
-        sizeof(so_linger)
-    );
-    if (status != 0){
-        perror("(Client) An error occured while setting the socket options");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
+  // Set sock options to specify Reno or Cubic
+  status = setsockopt(sockfd, IPPROTO_TCP, TCP_CONGESTION, TCP_CONG_TYPE,
+                      strlen(TCP_CONG_TYPE));
+  if (status != 0) {
+    perror("(Client) An error occured while setting the socket options");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
 
-    /* Create the TCP Connection */
+  // Set the SO_LINGER sock option
+  struct linger so_linger;
+  so_linger.l_onoff = 1;
+  so_linger.l_linger = 10;
+  status =
+      setsockopt(sockfd, SOL_SOCKET, SO_LINGER, &so_linger, sizeof(so_linger));
+  if (status != 0) {
+    perror("(Client) An error occured while setting the socket options");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
 
-    // Create the struct for the server's address:
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(RECV_PORT);
-    server_addr.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+  /* Create the TCP Connection */
 
-    status = connect(
-        sockfd, 
-        (struct sockaddr*) &server_addr, 
-        sizeof(server_addr)
-    );
-    if(status != 0){
-        perror("(Client) Error in connecting to the server");
-        close(sockfd);
-        exit(EXIT_FAILURE);
-    }
-    printf("(Client) Connected to the server\n");
+  // Create the struct for the server's address:
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(RECV_PORT);
+  server_addr.sin_addr.s_addr = inet_addr(LOCAL_HOST);
 
+  status =
+      connect(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (status != 0) {
+    perror("(Client) Error in connecting to the server");
+    close(sockfd);
+    exit(EXIT_FAILURE);
+  }
+  printf("(Client) Connected to the server\n");
 
-    /* File Transmisson */
+  /* File Transmisson */
 
-    // Load in the file to send
-    FILE *f;
-    f = fopen(PATH_SEND, "r");
-    if (f == NULL) {
-        perror("(Client) Error while reading the file");
-        exit(EXIT_FAILURE);
-    }
+  // Load in the file to send
+  FILE *f;
+  f = fopen(PATH_SEND, "r");
+  if (f == NULL) {
+    perror("(Client) Error while reading the file");
+    exit(EXIT_FAILURE);
+  }
 
-    // Send file:
-    
-    // Using sendfile
-    struct stat file_stat;
-    // Load file description
-    int fd = fileno(f);
-    status = fstat(fd, &file_stat); 
-    if (status < 0){
-        perror("(Client) Error while getting file description");
-        exit(EXIT_FAILURE);
-    }
-    printf("(Client) File Size: %ld bytes\n", file_stat.st_size);
+  // Send file:
 
-    int bytes_sent = sendfile(sockfd, fd, 0, file_stat.st_size);
-    if (bytes_sent < 0){
-        perror("(Client) Error while sending the file");
-        exit(EXIT_FAILURE);
-    }
-    else{
-        printf("(Client) %d bytes sent\n", bytes_sent);
-    }
+  // Using sendfile
+  struct stat file_stat;
+  // Load file description
+  int fd = fileno(f);
+  status = fstat(fd, &file_stat);
+  if (status < 0) {
+    perror("(Client) Error while getting file description");
+    exit(EXIT_FAILURE);
+  }
+  printf("(Client) File Size: %ld bytes\n", file_stat.st_size);
 
-    printf("(Client) Successfully sent the file\n");
-    fclose(f);
+  int bytes_sent = sendfile(sockfd, fd, 0, file_stat.st_size);
+  if (bytes_sent < 0) {
+    perror("(Client) Error while sending the file");
+    exit(EXIT_FAILURE);
+  } else {
+    printf("(Client) %d bytes sent\n", bytes_sent);
+  }
 
-    status = shutdown(sockfd, SHUT_WR); //close(sockfd); // Reference: https://stackoverflow.com/questions/25476348/c-sockets-incomplete-file-transfer
-    if (status == 0)
-        printf("(Client) Connection successfully shutdown\n");
-    else
-        perror("(Client) Error while closing the socket");
+  printf("(Client) Successfully sent the file\n");
+  fclose(f);
 
-    return 0;
+  status = shutdown(
+      sockfd,
+      SHUT_WR); // close(sockfd); // Reference:
+                // https://stackoverflow.com/questions/25476348/c-sockets-incomplete-file-transfer
+  if (status == 0)
+    printf("(Client) Connection successfully shutdown\n");
+  else
+    perror("(Client) Error while closing the socket");
 
+  return 0;
 }
--- src/server.c
+++ src/server.c
@@ -1,179 +1,166 @@
-#include <time.h> // For setting seeds for random numbers
-#include <stdio.h> // For IO operations
+#include <stdio.h>  // For IO operations
 #include <stdlib.h> // Standard Library
-#include <unistd.h> // Standard POSIX functions, like "close"
 #include <string.h> // String manipulation for string packets etc
-#include <sys/time.h> // For timeval struct
+#include <time.h>   // For setting seeds for random numbers
+#include <unistd.h> // Standard POSIX functions, like "close"
+
 #include <sys/socket.h> // For socket programming
+#include <sys/time.h>   // For timeval struct
+
 #include <arpa/inet.h> // Provides definitions for internet operations
 
 /* Constants */
-int status; // Used for error handling
+int status;            // Used for error handling
 const int SIZE = 4096; // In bytes
-const char* LOCAL_HOST = "127.0.0.1"; // Standard address for IPv4 loopback traffic
+const char *LOCAL_HOST =
+    "127.0.0.1"; // Standard address for IPv4 loopback traffic
 // int const SIZE = 1024; // gives a weird error for some reason
 int RECV_PORT = 5432;
 char *PATH_RECV = "recv.txt";
 const int MAX_PENDING = 10;
 
-struct timeval diffTimeval(struct timeval t2, struct timeval t1){
+struct timeval diffTimeval(struct timeval t2, struct timeval t1) {
 
-    // Reference for this function: https://stackoverflow.com/questions/32534392/timing-response-time-from-server
-    
-    struct timeval result;
+  // Reference for this function:
+  // https://stackoverflow.com/questions/32534392/timing-response-time-from-server
 
-    result.tv_sec = t2.tv_sec - t1.tv_sec;      // subtract seconds
-    result.tv_usec = t2.tv_usec - t1.tv_usec;   // subtract microseconds
+  struct timeval result;
 
-    // microsecond result could be negative, ex. 2.1 - 1.9 = 1 sec - 800000 microseconds
-    // if so, subtract one second and add 1000000 microseconds
-    while (result.tv_usec < 0) {
-        result.tv_usec += 1000000;
-        result.tv_sec--;
-    }
+  result.tv_sec = t2.tv_sec - t1.tv_sec;    // subtract seconds
+  result.tv_usec = t2.tv_usec - t1.tv_usec; // subtract microseconds
 
-    return result;
-    
+  // microsecond result could be negative, ex. 2.1 - 1.9 = 1 sec - 800000
+  // microseconds if so, subtract one second and add 1000000 microseconds
+  while (result.tv_usec < 0) {
+    result.tv_usec += 1000000;
+    result.tv_sec--;
+  }
+
+  return result;
 }
 
-int main(){
-    
-    /* Create a TCP socket */
+int main() {
 
-    // Passive open
-    int sockfd = socket(
-        AF_INET, // IPv4
-        SOCK_STREAM, // Specifies TCP socket
-        IPPROTO_TCP // TCP protocol, setting this to 0 also works
-    );
-    if (sockfd < 0){
-        perror("(Server) Error while creating the socket");
-        exit(EXIT_FAILURE);
+  /* Create a TCP socket */
+
+  // Passive open
+  int sockfd = socket(AF_INET,     // IPv4
+                      SOCK_STREAM, // Specifies TCP socket
+                      IPPROTO_TCP  // TCP protocol, setting this to 0 also works
+  );
+  if (sockfd < 0) {
+    perror("(Server) Error while creating the socket");
+    exit(EXIT_FAILURE);
+  }
+  printf("(Server) Socket created successfully\n");
+
+  // Set sock options to completely close the socket, for immediate reuse:
+  int reuse_flag = 1;
+  status = setsockopt(
+      // The socket:
+      sockfd,
+      // The socket layer, more info:
+      // https://stackoverflow.com/questions/21515946/what-is-sol-socket-used-for
+      SOL_SOCKET,
+      // This option allows your server to bind to an address which is in a
+      // TIME_WAIT state
+      SO_REUSEADDR,
+      // Setting to true
+      &reuse_flag, sizeof(int));
+  if (status != 0) {
+    perror("(Server) An error occured while setting the socket options");
+    exit(EXIT_FAILURE);
+  }
+
+  status =
+      setsockopt(sockfd, SOL_SOCKET, SO_REUSEPORT, &reuse_flag, sizeof(int));
+  if (status != 0) {
+    perror("(Server) An error occured while setting the socket options");
+    exit(EXIT_FAILURE);
+  }
+
+  /* Create the TCP Connection */
+
+  // Create the struct for the server's address:
+  struct sockaddr_in server_addr;
+  memset(&server_addr, 0, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  server_addr.sin_port = htons(RECV_PORT);
+  server_addr.sin_addr.s_addr = inet_addr(LOCAL_HOST);
+
+  // Passive open: Bind server to server's port:
+  status = bind(sockfd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+  if (status < 0) {
+    perror("(Server) Error while binding");
+    exit(EXIT_FAILURE);
+  }
+  printf("(Server) Bind successful\n");
+
+  status = listen(sockfd, MAX_PENDING);
+  if (status == 0)
+    printf("(Server) Listening...\n");
+  else {
+    perror("(Server) Unable to listen");
+    exit(EXIT_FAILURE);
+  }
+
+  /* File Transmisson */
+
+  struct sockaddr_in client_addr;
+  memset(&client_addr, 0, sizeof(client_addr));
+  socklen_t len_caddr = sizeof(client_addr);
+  int new_sockfd = accept(sockfd, (struct sockaddr *)&client_addr, &len_caddr);
+  if (new_sockfd < 0) {
+    perror("(Server) Error while creating the new socket");
+    exit(EXIT_FAILURE);
+  }
+
+  // Create the file where the contents will be saved
+  FILE *f;
+  f = fopen(PATH_RECV, "w+");
+
+  char buffer[SIZE];
+  bzero(buffer, sizeof(buffer));
+
+  // t1: file transfer started
+  struct timeval t1;
+  gettimeofday(&t1, NULL);
+
+  while (1) {
+    status = recv(new_sockfd, buffer, sizeof(buffer) - 1, 0);
+    if (status == 0) {
+      printf("(Server) File contents saved successfully\n");
+      break;
+    } else if (status < 0) {
+      perror("(Server) Error while recieving file contents");
+      exit(EXIT_FAILURE);
+    } else {
+      // printf("(Server) Recieved %d bytes\n", status);
+      fprintf(f, "%s", buffer);
     }
-    printf("(Server) Socket created successfully\n");
-
-    // Set sock options to completely close the socket, for immediate reuse:
-    int reuse_flag = 1;
-    status = setsockopt(
-        // The socket:
-        sockfd,
-        // The socket layer, more info: https://stackoverflow.com/questions/21515946/what-is-sol-socket-used-for
-        SOL_SOCKET,
-        // This option allows your server to bind to an address which is in a TIME_WAIT state 
-        SO_REUSEADDR,
-        // Setting to true
-        &reuse_flag,
-        sizeof(int)
-    );
-    if (status != 0){
-        perror("(Server) An error occured while setting the socket options");
-        exit(EXIT_FAILURE);
-    }
-
-    status = setsockopt(
-        sockfd,
-        SOL_SOCKET,
-        SO_REUSEPORT,
-        &reuse_flag,
-        sizeof(int)
-    );
-    if (status != 0){
-        perror("(Server) An error occured while setting the socket options");
-        exit(EXIT_FAILURE);
-    }
-
-    /* Create the TCP Connection */
-
-    // Create the struct for the server's address:
-    struct sockaddr_in server_addr;
-    memset(&server_addr, 0, sizeof(server_addr));
-    server_addr.sin_family = AF_INET;
-    server_addr.sin_port = htons(RECV_PORT);
-    server_addr.sin_addr.s_addr = inet_addr(LOCAL_HOST);
-
-    // Passive open: Bind server to server's port:
-    status = bind(
-        sockfd, 
-        (struct sockaddr*) &server_addr, 
-        sizeof(server_addr)
-    );
-    if(status < 0) {
-        perror("(Server) Error while binding");
-        exit(EXIT_FAILURE);
-    }
-    printf("(Server) Bind successful\n");
-
-    status = listen(sockfd, MAX_PENDING);
-    if(status == 0)
-        printf("(Server) Listening...\n");
-    else{
-        perror("(Server) Unable to listen");
-        exit(EXIT_FAILURE);
-    }
-
-    /* File Transmisson */
-
-    struct sockaddr_in client_addr;
-    memset(&client_addr, 0, sizeof(client_addr));
-    socklen_t len_caddr = sizeof(client_addr);
-    int new_sockfd = accept(
-        sockfd, 
-        (struct sockaddr*) &client_addr, 
-        &len_caddr
-    );
-    if (new_sockfd < 0){
-        perror("(Server) Error while creating the new socket");
-        exit(EXIT_FAILURE);
-    }
-    
-    // Create the file where the contents will be saved
-    FILE *f;
-    f = fopen(PATH_RECV, "w+");
-
-    char buffer[SIZE];
+    // printf("(Server) %s\n", buffer);
     bzero(buffer, sizeof(buffer));
+  }
 
-    // t1: file transfer started
-    struct timeval t1;
-    gettimeofday(&t1, NULL);
+  // t2: file transfer ended
+  struct timeval t2;
+  gettimeofday(&t2, NULL);
 
-    while (1){
-        status = recv(new_sockfd, buffer, sizeof(buffer) - 1, 0);
-        if (status == 0){
-            printf("(Server) File contents saved successfully\n");
-            break;
-        }
-        else if (status < 0){
-            perror("(Server) Error while recieving file contents");
-            exit(EXIT_FAILURE);
-        }
-        else{
-            // printf("(Server) Recieved %d bytes\n", status);
-            fprintf(f, "%s", buffer);
-        }
-        // printf("(Server) %s\n", buffer);
-        bzero(buffer, sizeof(buffer));
-    }
+  fclose(f);
 
-    // t2: file transfer ended
-    struct timeval t2;
-    gettimeofday(&t2, NULL);
+  int status1 = close(new_sockfd);
+  int status2 = close(sockfd);
 
-    fclose(f);
+  if (status1 == 0 && status2 == 0)
+    printf("(Server) Connection successfully closed\n");
+  else
+    perror("(Server) Error while closing the socket");
 
-    int status1 = close(new_sockfd);
-    int status2 = close(sockfd);
+  printf("(Common) File transfer complete\n");
 
-    if (status1 == 0 && status2 == 0)
-        printf("(Server) Connection successfully closed\n");
-    else
-        perror("(Server) Error while closing the socket");
+  struct timeval time_file = diffTimeval(t2, t1);
+  printf("\nTime taken for the file transfer: %ld.%06ld s\n", time_file.tv_sec,
+         time_file.tv_usec);
 
-    printf("(Common) File transfer complete\n");
-
-    struct timeval time_file = diffTimeval(t2, t1);
-    printf("\nTime taken for the file transfer: %ld.%06ld s\n", time_file.tv_sec, time_file.tv_usec);
-
-    return 0;
-
+  return 0;
 }
